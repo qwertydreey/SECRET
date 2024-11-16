2c57f154063613000812d46a5d939baf24c2fb74
qwertydreey <andreitan598@gmail.com>
2024-11-16 21:22:05 +0800

    q

---
+++ script.js
@@ -0,0 +1,4 @@
+// Example: Alert when the user clicks on Contact link
+document.querySelector('a[href="#contact"]').addEventListener('click', () => {
+    alert('Thank you for visiting the Contact section!');
+});
